9dbb6b6fb6f1befa3083ceaf6591f86d9bd6baae
Vinícius Lima <villylss@gmail.com>
2022-11-09 19:13:14 -0300

    feature: scrips js feito

---
+++ campos.js
@@ -0,0 +1,17 @@
+const campos = [
+    {
+        name: 'Cobal',
+        coordinates: [-22.86750880445259, -43.21829123227059],
+        description: 'ola mundo',
+    }, 
+    {
+        name: 'Cobal Pequeno',
+        coordinates: [-22.867392357401517, -43.21910809165156],
+        description: '',
+    },
+    {
+        name: 'Coppe Grande',
+        coordinates: [-22.863866, -43.229407],
+        description: '',
+    },
+]
