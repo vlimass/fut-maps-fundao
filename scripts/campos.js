a71c29d020d2e1eb444ac756caa433e15497fa04
lucas <lucaslyra47@gmail.com>
2022-11-09 21:16:19 -0300

    Refactor usability tests. Co-authored-by: Daniel Arruda danielp.arruda@gmail.com Co-authored-by: Vinícius Lima villylss@gmail.com

--- scripts/campos.js
+++ scripts/campos.js
@@ -3,90 +3,105 @@ const campos = [
         name: 'Cobal',
         coordinates: [-22.86750880445259, -43.21829123227059],
         description: 'ola mundo',
-        type: 'Campo'
+        type: 'Campo',
+        availability: 'Pago'
     }, 
     {
         name: 'Cobal Pequeno',
         coordinates: [-22.867392357401517, -43.21910809165156],
         description: '',
-        type: 'Campo'
+        type: 'Campo',
+        availability: 'Agendamento'
     },
     {
         name: 'Coppe Grande',
         coordinates: [-22.863365, -43.229168],
         description: '',
-        type: 'Campo'
+        type: 'Campo',
+        availability: 'Livre'
     },
     {
         name: 'Coppe Pequeno',
         coordinates: [-22.863866, -43.229407],
         description: '',
-        type: 'Campo'
+        type: 'Campo',
+        availability: 'Livre'
     },
     {
         name: 'FAU',
         coordinates: [-22.863655776302856, -43.224006379550644],
         description: '',
-        type: 'Campo'
+        type: 'Campo',
+        availability: 'Livre'
     },
     {
         name: 'Letras',
         coordinates: [-22.85980376890636, -43.224246008361234],
         description: '',
-        type: 'Campo'
+        type: 'Campo',
+        availability: 'Livre'
     },
     {
         name: 'Prefeitura 1',
         coordinates: [-22.848574, -43.237105],
         description: '',
-        type: 'Campo'
+        type: 'Campo',
+        availability: 'Livre'
     },
     {
         name: 'Prefeitura 2',
         coordinates: [-22.849018, -43.238036],
         description: '',
-        type: 'Campo'
+        type: 'Campo',
+        availability: 'Livre'
     },
     {
         name: 'Educação Física',
         coordinates: [-22.84191539514563, -43.23023840460363],
         description: '',
-        type: 'Campo'
+        type: 'Campo',
+        availability: 'Livre'
     },
     {
         name: 'Quadras Poliesportivas',
         coordinates: [-22.842700697560097, -43.230345639387636],
         description: '',
-        type: 'Quadra'
+        type: 'Quadra',
+        availability: 'Livre'
     },
     {
         name: 'CEPEL',
         coordinates: [-22.852978697137218, -43.23580052232401],
         description: '',
-        type: 'Campo'
+        type: 'Campo',
+        availability: 'Livre'
     },
     {
         name: 'CCMN',
         coordinates: [-22.85911801973079, -43.23351914511036],
         description: '',
-        type: 'Campo'
+        type: 'Campo',
+        availability: 'Livre'
     },
     {
         name: 'IEN',
         coordinates: [-22.86513707875586, -43.22796562939208],
         description: '',
-        type: 'Campo'
+        type: 'Campo',
+        availability: 'Livre'
     },
     {
         name: 'Quadras Poliesportivas 2',
         coordinates: [-22.842904946247383, -43.23206557241575],
         description: '',
-        type: 'Quadra'
+        type: 'Quadra',
+        availability: 'Livre'
     },
     {
         name: 'Alojamento Estudantil',
         coordinates: [-22.839027261281725, -43.22886323641195],
         description: '',
-        type: 'Campo'
+        type: 'Campo',
+        availability: 'Livre'
     }
 ]
